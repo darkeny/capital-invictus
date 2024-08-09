5e880aca31efea0ff055ee34fb98bde9602e0596
darkeny <darkenpaulo00@gmail.com>
2024-08-09 09:45:11 +0200

    update: implements constants values

---
+++ api/src/constants/error-messages.ts
@@ -0,0 +1,38 @@
+const failedRetrieval = "Failed to retrieve item";
+
+const notFound = "No items found";
+
+const requiredFields = "All fields are required";
+
+const errorCreating = "Error creating item";
+
+const duplicatedData = "Item with this identifier already exists";
+
+const wrong = "Something went wrong.";
+
+const requiredUpload = "Please upload a PDF file.";
+
+const failedDeletion = "Error deleting item";
+
+const invalidCredentials = "Invalid email or password.";
+
+const errorLogin = "An error occurred while logging in.";
+
+const duplicateEmail = 'O email já foi utilizado para inscrição.';
+
+
+const ERROR_MESSAGES = {
+    errorLogin,
+    duplicateEmail,
+    invalidCredentials,
+    duplicatedData,
+    failedRetrieval,
+    requiredFields,
+    errorCreating,
+    notFound,
+    wrong,
+    requiredUpload,
+    failedDeletion
+}
+
+export default ERROR_MESSAGES;
