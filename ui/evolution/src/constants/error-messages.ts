ac4e974f0f7839d9b10488af022031813701f5f1
darkeny <darkenpaulo00@gmail.com>
2024-09-06 21:26:26 +0200

    update: configure ui constantss

---
+++ ui/evolution/src/constants/error-messages.ts
@@ -0,0 +1,32 @@
+const failedRetrieval = "Failed to retrieve item";
+
+const notFound = "No items found";
+
+const requiredFields = "All fields are required";
+
+const errorCreating = "Error creating item";
+
+const duplicatedData = "Item with this identifier already exists";
+
+const wrong = "Something went wrong.";
+
+const requiredUpload = "Please upload a PDF file.";
+
+const duplicateEmail = 'O email já foi utilizado para inscrição.';
+
+
+
+
+
+const ERROR_MESSAGES = {
+    duplicatedData,
+    duplicateEmail,
+    failedRetrieval,
+    requiredFields,
+    errorCreating,
+    notFound,
+    wrong,
+    requiredUpload
+}
+
+export default ERROR_MESSAGES;
