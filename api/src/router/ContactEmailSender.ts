b093c8749c03067658dbc17f1f6b7ccdc79b621d
darkeny <darkenpaulo00@gmail.com>
2024-09-23 20:19:46 +0200

    update: implementing new model to loan

--- api/src/router/ContactEmailSender.ts
+++ api/src/router/ContactEmailSender.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as controller from '../controllers/ContactEmailSender';
+import * as controller from '../controllers/contactEmailSender';
 
 const router = Router();
 
--- api/src/router/IbuildRouter.ts
+++ api/src/router/IbuildRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as controller from '../controllers/ibuild';
+import * as controller from '../controllers/ibuildSignup';
 
 const router = Router();
 
