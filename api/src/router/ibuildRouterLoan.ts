3453bdb5e7aa65a68ed3e6772b11a6e7710d7f29
darkeny <darkenpaulo00@gmail.com>
2024-10-07 20:00:56 +0200

    update: implementing penhor router

--- api/src/router/ibuildRouterLoan.ts
+++ api/src/router/ibuildRouterLoan.ts
@@ -7,6 +7,6 @@ router.post('/', controller.IBuildLoan);
 router.get('/', controller.getAll);
 router.delete('/:id', controller.deleteLoan);
 router.put('/:id', controller.updateLoan);
-router.put('/:id', controller.updatePawnStatus);
+router.put('/pawn/:id', controller.updatePawnStatus);
 
 export { router as IbuildRouterLoan };
