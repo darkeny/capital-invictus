2c960b9f127758701d5f905597d17ecb69950a2b
darkeny <darkenpaulo00@gmail.com>
2024-11-13 21:21:38 +0200

    update: implementing email sender when client request a loan

---
+++ api/src/router/LoansEmailSender.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import RegisterController from "../controllers/LoansEmailSender/mailConfig";
+
+const router = Router();
+
+router.post('/', async (req, res) => {
+    const { email, fullName } = req.body;
+
+    console.log('Received Data:', { email, fullName });
+
+    const registerController = new RegisterController();
+
+    try {
+        await registerController.registerUser(email, fullName);
+
+        res.status(200).send({ message: 'Email sent successfully.' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send({ message: 'Failed to send email.' });
+    }
+});
+
+export { router as SendMessageToClient };
+
--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -8,19 +8,21 @@ import { NewsletterRouter } from "./Newsletter";
 import { UserLogin } from "./ClientsLogin";
 import { UserAuthRouter } from "./UserRoutes";
 import { SendMessage } from "./ContactEmailSender";
+import { SendMessageToClient } from "./LoansEmailSender";
 
 
 
 const ServerRouters = (server: Express): void => {
+    server.use('/SendMail', SendMail)
+    server.use('/sendLoansMail', SendMessageToClient)
     server.use('/sendMessage', SendMessage)
     server.use('/ibuildCustomer', IbuildRouter) // endeponit dos usuarios
-    server.use('/SendMail', SendMail)
     server.use('/ibuildLoan', IbuildRouterLoan) // endeponit dos emprestimos dos usuarios
     server.use('/admin/users', AdminRouter)
-    server.use('/admin/login', AdminLogin) 
+    server.use('/admin/login', AdminLogin)
     server.use('/user/login', UserLogin) // endeponit de login que leva os usuarios para o painel deles
     server.use('/newsletter', NewsletterRouter);
-    server.use('/me', UserAuthRouter);    
+    server.use('/me', UserAuthRouter);
 };
 
 export default ServerRouters;
